ffd2106eec3d9df49f86503ad22fcfac751e06ea
Filipp Mun <noghtguards@gmail.com>
2021-11-17 23:16:02 +0600

    4555

--- 555.js
+++ 555.js
@@ -1,8 +1,1 @@
-const Animal = function (options) {
-  this.name = options.name;
-  this.color = options.color;
-};
-
-const dog = new Animal({ name: 'Rex' });
-
-console.log(dog);
+console.log('Hi' + 'JS');
